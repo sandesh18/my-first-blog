e3e6c33d09346bce7ab78ca9619b85583e57d932
sandesh <sandesh181995@gmail.com>
2016-05-25 16:28:24 +0530

    only Html.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 
-from .models import Post
-
 def post_list(request):
-    posts = Post.objects.filter(title__contains='a')
-    return render(request, 'blog/post_list.html', {'posts':posts})
+        return render(request, 'blog/post_list.html', {})
